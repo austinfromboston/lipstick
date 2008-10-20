72df56f8d16d6c671ad4fd43b0dd9dacbb266d7e
Austin Putman <austin@radicaldesigns.org>
2008-10-19 19:43:14 -0700

    remote organizations create

--- app/helpers/linein_urls_helper.rb
+++ app/helpers/linein_urls_helper.rb
@@ -2,6 +2,12 @@ module LineinUrlsHelper
   def new_person_linein_path( options = {} )
     Linein::CONFIG[:aquarius][:json_url] + '/people/new'
   end
+  def edit_person_linein_path( contact )
+    Linein::CONFIG[:aquarius][:json_url] + "/people/#{contact.person_id}/edit"
+  end
+  def new_organization_linein_path( options = {} )
+    Linein::CONFIG[:aquarius][:json_url] + '/organizations/new'
+  end
   def new_organization_person_linein_path( org, options = {} )
     Linein::CONFIG[:aquarius][:json_url] + "/organizations/#{ org.to_param }/people/new"
   end
